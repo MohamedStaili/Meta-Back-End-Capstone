d806a969553bdf3940f3f046a053069311d2b001
MohamedStaili <5254mohamed@gmail.com>
2025-10-04 18:22:48 +0100

    create models booking and menu

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,11 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'capstoneBackEnd',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1977'
     }
 }
 
